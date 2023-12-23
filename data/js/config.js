6a0392c0906dc686e0a07dba2f14f191b8b8383a
Prakhar Doneria <gamesprotec@gmail.com>
2023-12-23 12:29:11 +0530

    Create config.js

---
+++ data/js/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC-ZtQ4N3d5ZYZcUCETe0quVgn3wD8Gdq4",
+    authDomain: "datagram-in.firebaseapp.com",
+    databaseURL: "https://datagram-in-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "datagram-in",
+    storageBucket: "datagram-in.appspot.com",
+    messagingSenderId: "984986151374",
+    appId: "1:984986151374:web:8428c23ad9e8bd88ee639f"
+};
